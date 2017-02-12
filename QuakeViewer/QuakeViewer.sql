6dff3192a8ea5bde5bcc7f54cd09bddacca3abdb
araces <jiangshang2012@live.com>
2017-02-12 17:39:27 +0800

    update sql base

--- QuakeViewer/QuakeViewer.sql
+++ QuakeViewer/QuakeViewer.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50717
  File Encoding         : utf-8
 
- Date: 02/09/2017 14:58:10 PM
+ Date: 02/12/2017 17:38:43 PM
 */
 
 SET FOREIGN_KEY_CHECKS = 0;
@@ -29,8 +29,16 @@ CREATE TABLE `Accounts` (
   `LastLoginDate` datetime DEFAULT NULL,
   `Status` int(11) DEFAULT NULL,
   `AccountType` int(11) DEFAULT '1' COMMENT '1:用户，2:管理员',
+  `Mobile` varchar(11) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ----------------------------
+--  Records of `Accounts`
+-- ----------------------------
+BEGIN;
+INSERT INTO `Accounts` VALUES ('4750713371772938697', 'araces', '7C4A8D09CA3762AF61E59520943DC26494F8941B', '2017-02-11 20:12:43', '1', '2017-02-11 20:12:43', '1', '2', '18612556662');
+COMMIT;
 
 -- ----------------------------
 --  Table structure for `AreaParams`
@@ -54,11 +62,12 @@ CREATE TABLE `Choices` (
   `Id` varchar(20) COLLATE utf8_bin NOT NULL,
   `UserId` varchar(20) COLLATE utf8_bin NOT NULL,
   `UserName` varchar(50) COLLATE utf8_bin DEFAULT NULL,
-  `FirstChoice` varchar(10) COLLATE utf8_bin DEFAULT NULL,
-  `SecondChoice` varchar(10) COLLATE utf8_bin DEFAULT NULL,
-  `ThirdChoice` varchar(10) COLLATE utf8_bin DEFAULT NULL,
-  `ForthChoice` varchar(10) COLLATE utf8_bin DEFAULT NULL,
-  `FifthChoice` varchar(10) COLLATE utf8_bin DEFAULT NULL,
+  `FirstChoice` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `SecondChoice` int(10) DEFAULT NULL,
+  `ThirdChoice` int(10) DEFAULT NULL,
+  `ForthChoice` int(10) DEFAULT NULL,
+  `FifthChoice` int(10) DEFAULT NULL,
+  `Sixth` int(11) DEFAULT NULL,
   `FromType` int(11) DEFAULT NULL,
   `MinorResult` int(11) DEFAULT NULL,
   `MajorResult` int(11) DEFAULT NULL,
