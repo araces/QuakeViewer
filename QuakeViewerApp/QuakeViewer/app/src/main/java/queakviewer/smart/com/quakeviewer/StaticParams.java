d196e419594fe28503e82dc7eea5c73e48142aff
Ares.Zhao <jiangshang2012@live.com>
2017-03-13 21:37:58 +0800

    no message

--- QuakeViewerApp/QuakeViewer/app/src/main/java/queakviewer/smart/com/quakeviewer/StaticParams.java
+++ QuakeViewerApp/QuakeViewer/app/src/main/java/queakviewer/smart/com/quakeviewer/StaticParams.java
@@ -5,16 +5,16 @@ package queakviewer.smart.com.quakeviewer;
  */
 
 public class StaticParams {
-/*
+
     public final static String REGIST_URL="http://10.44.7.22/api/regist";
     public final static String LOGIN_URL="http://10.44.7.22/api/login";
     public final static String QUESTION_URL="http://10.44.7.22/api/QuestionsResult";
     public final static String AREAS_URL="http://10.44.7.22/api/GetAllProvince?token=";
-    */
+/*
     //public final static String AREAS_URL="http://192.168.0.134/api/GetAllProvince?token=";
     public final static String REGIST_URL="http://192.168.0.134/api/regist";
     public final static String LOGIN_URL="http://192.168.0.134/api/login";
     public final static String QUESTION_URL="http://192.168.0.134/api/QuestionsResult";
     public final static String AREAS_URL="http://192.168.0.134/api/GetAllProvince?token=";
-
+   */
 }
